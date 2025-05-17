7e813c4c5cdcac38cd032a7d87f011fcdfd5b73e
Eric Ene <eric03ene@gmail.com>
2025-05-17 16:40:45 -0400

    fixed bugs in git status stuff for fish prompt, related to HEAD and origin stuff

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -9,9 +9,10 @@ function git_info --description 'get git inof'
         return
     end
 
-    set -l branch $(git rev-parse --abbrev-ref HEAD)
+    set -l branch $(git symbolic-ref --short HEAD)
+    
+    set -l dirty $(git status --porcelain | wc -l)
 
-    set -l dirty $(git diff-index HEAD | wc -l)
     set -l color
 
     if test $dirty -eq 0
@@ -20,8 +21,15 @@ function git_info --description 'get git inof'
         set color red
     end
 
-    set -l commits $(git rev-list --left-right --count origin/main...main | sed -E 's/([0-9]+).*([0-9]+)/\2\n\1/g')
+    set -l commits_raw $(git rev-list --left-right --count origin/$branch...$branch 2> /dev/null)
+    set -l _status $status
 
+    if test $_status -ne 0
+        printf " [$(color_str $branch $color)]"
+        return
+    end
+
+    set -l commits $(echo $commits_raw | sed -E 's/([0-9]+).*([0-9]+)/\2\n\1/g'; or "0\n0")
     set -l commit_str ""
 
     if test $commits[1] -ne 0
