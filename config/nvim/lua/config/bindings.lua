a45beefeca9bf046b1b48b0d4a43d2fd4bcc03d3
Eric Ene <eric03ene+git@gmail.com>
2025-08-14 18:43:41 -0400

    nvim minning

--- config/nvim/lua/config/bindings.lua
+++ config/nvim/lua/config/bindings.lua
@@ -14,6 +14,8 @@ map.set(mode, "<C-l>", "<C-w>l")
 map.set(mode, "<C-j>", "<C-w>j")
 map.set(mode, "<C-k>", "<C-w>k")
 
+map.set(mode, "<Leader>a", vim.lsp.buf.code_action)
+
 map.set(mode, "<Leader>c", "<Plug>(comment_toggle_linewise_current)")
 
 map.set(mode, "<Leader>f", "<cmd>Telescope find_files<cr>")
